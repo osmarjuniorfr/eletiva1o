6f0c53fe2e7f8bb28170ddbe1df2dc65f09dec83
Osmar <osmarjuniorfr@gmail.com>
2025-08-31 14:42:00 -0300

    finalizando exercicio 02 lista 01 subtração de valores com metodo POST

---
+++ lista01/exercicio02/exercicio02.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <title>Lista 01 - Exercício 02</title>
+</head>
+<body>
+
+    <main class="container">
+        <h1>Subtração do segundo número menos o primeiro</h1>
+        <form class="border p-4" method="post" action="respostaexercicio02.php">
+            <div class="row">
+                <div class="col-md-2 mt-3">
+                    <label for id="num1" class="form-label">Primeiro número:</label>
+                    <input type="text" name="num1" id="num1" class="form-control"> 
+                </div>
+
+                <div class="col-md-2 mt-3">
+                    <label for id="num2" class="form-label">Segundo número:</label>
+                    <input type="text" name="num2" id="num2" class="form-control">
+                </div>
+            </div>
+
+            <input type="submit" value="Subtrair" class="mt-3">
+        </form>
+    </main>
+</body>
+</html>
